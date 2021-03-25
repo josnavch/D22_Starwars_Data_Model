0ad6b87b3fbcc4d0e303c2840b454bf3c10d181f
josnavch <josnavch@gmail.com>
2021-03-25 02:49:08 +0000

    finish model DB Star Wars JN

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,24 +8,81 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__ = 'Planets'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column( String(50))
+    rotation_period = Column( Integer , nullable=False)
+    orbital_period = Column( Integer, nullable=False)
+    diameter = Column( Integer, nullable=False)
+    climate = Column( String(200), nullable=False)
+    gravity = Column( String(200), nullable=False)
+    terrain = Column( String(200), nullable=False)
+    surface_water = Column( Integer, nullable=False)
+    population = Column( Integer, nullable=False)
+    url = Column( String(200), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class People(Base):
+    __tablename__ = 'People'
+    id = Column( Integer, primary_key=True)
+    name = Column( String(50), nullable=True)
+    height = Column( Integer, nullable=False)
+    mass = Column( Integer, nullable=False)
+    hair_color = Column( String(200), nullable=False)
+    skin_color = Column( String(200), nullable=False)
+    eye_color = Column( String(200), nullable=False)
+    birth_year = Column( String(200), nullable=False)
+    gender = Column( String(200), nullable=False)
+    homeworld = Column( String(200), nullable=False)
+    url = Column( String(200), nullable=False)
 
+
+class Starships(Base):
+    __tablename__ = 'Starships'
+    id = Column( Integer, primary_key=True)
+    name = Column( String(50), nullable=True)
+    model = Column( String(200), nullable=False)
+    manufacturer = Column( String(200), nullable=False)
+    cost_in_credits = Column( Integer, nullable=False)
+    length = Column( Integer, nullable=False)
+    max_atmosphering_speed = Column( String(200), nullable=False)
+    crew = Column( Float, nullable=False)
+    passengers = Column( Integer, nullable=False)
+    cargo_capacity = Column( Integer, nullable=False)
+    consumables = Column( String(200), nullable=False)
+    hyperdrive_rating = Column( Integer, nullable=False)
+    MGLT = Column( Integer, nullable=False)
+    url = Column( String(200), nullable=False)
+
+
+class Species(Base):
+    __tablename__ = 'Species'
+    id = Column( Integer, primary_key=True)
+    name = Column( String(50), nullable=True)
+    classification = Column( String(200), nullable=False)
+    designation = Column( String(200), nullable=False)
+    average_height = Column( Integer, nullable=False)
+    skin_colors = Column( String(200), nullable=False)
+    hair_colors = Column( String(200), nullable=False)
+    eye_colors = Column( String(200), nullable=False)
+    average_lifespan = Column( Integer, nullable=False)
+    homeworld = Column( String(200), nullable=False)
+    language = Column( String(200), nullable=False)
+    url = Column( String(200), nullable=False)
+
+
+class Users(Base):
+    __tablename__ = 'Users'
+    id = Column( Integer, primary_key=True)
+    name = Column( String(200), nullable=False)
+    password = Column( String(22), nullable=False)
+
+class Favorites(Base):
+    __tablename__ = 'Favorites'
+    favoriteid = Column( Integer, primary_key=True)
+    userID = Column(Integer, ForeignKey('Users.id'))
+    type = Column( String(15), nullable=False)
+    
     def to_dict(self):
         return {}
 
